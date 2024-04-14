5391bf51899f5390fca7656707a2dab5f8f4d6f6
MicHaeLNgSd <misha.grigorov2020@gmail.com>
2024-04-14 20:27:45 +0300

    rename js to jsx + create Switch routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
+import { Switch, Route } from 'react-router-dom/cjs/react-router-dom';
 import './App.css';
+import Header from './components/Header';
+import ProfilePage from './pages/ProfilePage';
+import RegistrationPage from './pages/RegistrationPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Switch>
+      <Route exact path='/' component={Header} />
+      <Route path='/profile' component={ProfilePage} />
+      <Route path='/reg' component={RegistrationPage} />
+    </Switch>
   );
 }
 
