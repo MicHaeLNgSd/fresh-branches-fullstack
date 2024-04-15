ac9ee9e800514ee00a0d090f93aa320b2e3e0b60
MicHaeLNgSd <misha.grigorov2020@gmail.com>
2024-04-15 16:55:57 +0300

    add super basic profile

--- client/src/components/Header/index.jsx
+++ client/src/components/Header/index.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom/cjs/react-router-dom.min';
-import style from './Header.module.scss';
+import styles from './Header.module.scss';
 
 function Header() {
   return (
-    <header className={style.header}>
-      <ul className={style.nav}>
+    <header className={styles.header}>
+      <ul className={styles.nav}>
         <li>
           <NavLink
             to='/'
             exact
-            className={style.navLink}
-            activeClassName={style.selected}
+            className={styles.navLink}
+            activeClassName={styles.selected}
           >
             Home
           </NavLink>
@@ -19,14 +19,14 @@ function Header() {
         <li>
           <NavLink
             to='/profile'
-            className={style.navLink}
-            activeClassName={style.selected}
+            className={styles.navLink}
+            activeClassName={styles.selected}
           >
             Profile
           </NavLink>
         </li>
         <li>
-          <NavLink to='/reg' className={style.navLink} activeClassName={style.selected}>
+          <NavLink to='/reg' className={styles.navLink} activeClassName={styles.selected}>
             Registration
           </NavLink>
         </li>
--- client/src/pages/ProfilePage/index.jsx
+++ client/src/pages/ProfilePage/index.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import Header from '../../components/Header';
 import UserProfile from '../../components/UserProfile';
+import styles from './ProfilePage.module.scss';
 
 function ProfilePage({ userData: [user, setUser] }) {
   return (
     <>
       <Header />
-      <p>ProfilePage</p>
-      <button>Change</button>
-      <UserProfile user={user} />
+      <main className={styles.main}>
+        <h1 className={styles.startText}>Welcome to ProfilePage!</h1>
+        <UserProfile user={user} />
+      </main>
     </>
   );
 }
