8f6c458bc80e1e63007290e6df8c8c7ba0c24f15
MicHaeLNgSd <misha.grigorov2020@gmail.com>
2024-04-16 19:37:36 +0300

    add basic error handler

--- server/app.js
+++ server/app.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const rootRouter = require('./routers');
+const basicErrorHandler = require('./middlewares/errors');
 const app = express();
 
 app.use(express.json());
 app.use('/api', rootRouter);
-//TODO errors hendler
+app.use(basicErrorHandler);
 
 module.exports = app;
